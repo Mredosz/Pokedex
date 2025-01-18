91d03e927bfaed84c9752e041c8bfdcde4bbe316
mredo <mredosz4@gmail.com>
2025-01-18 17:46:55 +0100

    add second endpoint and repair

--- server/src/app.js
+++ server/src/app.js
@@ -1,62 +1,53 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
-const { spawn } = require("child_process");
+const {spawn} = require("child_process");
 const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({origin: "http://localhost:5173"}));
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({dest: "uploads/"});
+
+const sendData = (pythonProcess, res) => {
+    let result = '';
+    pythonProcess.stdout.on('data', (data) => {
+        result += data.toString();
+    });
+
+    pythonProcess.on('close', () => {
+        res.json(JSON.parse(result.trim()));
+    });
+
+    pythonProcess.stderr.on('data', (error) => {
+        console.error(`Error: ${error}`);
+        res.status(500).send('Error during prediction');
+    });
+}
 
 app.get("/metrics", (req, res) => {
-  const pythonProcess = spawn("python", ["../src/Metrics.py"]);
+    const pythonProcess = spawn("python", ["../src/Metrics.py"]);
 
-  let result = '';
-  pythonProcess.stdout.on('data', (data) => {
-    result += data.toString();
-  });
-
-  pythonProcess.on('close', () => {
-    res.json(JSON.parse(result.trim()));
-  });
-
-  pythonProcess.stderr.on('data', (error) => {
-    console.error(`Error: ${error}`);
-    res.status(500).send('Error during prediction');
-  });
+    sendData(pythonProcess, res);
 });
 
 
-
 app.post("/predict", upload.single("image"), (req, res) => {
-  if (!req.file) {
-    return res.status(400).send("No image file uploaded.");
-  }
+    if (!req.file) {
+        return res.status(400).send("No image file uploaded.");
+    }
 
-  const imagePath = path.resolve(req.file.path);
+    const imagePath = path.resolve(req.file.path);
 
-  const pythonProcess = spawn("python", ["../src/PredictImageForEachModel.py", imagePath]);
+    const pythonProcess = spawn("python", ["../src/PredictImageForEachModel.py", imagePath]);
 
-  let result = '';
-  pythonProcess.stdout.on('data', (data) => {
-    result += data.toString();
-  });
-
-  pythonProcess.on('close', () => {
-    res.json(JSON.parse(result.trim()));
-  });
-
-  pythonProcess.stderr.on('data', (error) => {
-    console.error(`Error: ${error}`);
-    res.status(500).send('Error during prediction');
-  });
+    sendData(pythonProcess, res);
 });
 
 
 app.listen(3000, () => {
-  console.log("Server listening at port 3000");
+    console.log("Server listening at port 3000");
 });
